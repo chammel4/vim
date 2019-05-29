8652d5cb7711e9f332d2521009b74f3eca082241
chammel4 <47669669+chammel4@users.noreply.github.com>
2019-05-29 15:37:35 -0500

    added putty installation

--- chocolatey_fresh_install.bat
+++ chocolatey_fresh_install.bat
@@ -12,6 +12,7 @@ choco install vim -fy
 choco install cmake -fy
 choco install sourcetree -fy
 choco install sql-server-management-studio -fy
+choco install putty -fy
 
 cd %USERPROFILE%
 git clone https://github.com/VundleVim/Vundle.vim.git:q %USERPROFILE%/vimfiles/bundle/Vundle.vim
