4afcc3bbf4b0254f9cec6729eae2663417a1c966
Chris Hammel <chammel4@gmail.com>
2019-11-16 22:44:43 +0100

    prompts added for type of install; global git config set, and vundle plugininstall called from script

--- chocolatey_fresh_install.bat
+++ chocolatey_fresh_install.bat
@@ -1,20 +1,49 @@
+@echo off
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install chocolateygui -fy
-choco install ctags -fy
-choco install curl -fy
-choco install git -fy
-choco install python -fy
+setlocal
+
+set /p BasicInstall="Basic dev install? (git, curl, ctags, vim, vundle, python)? (y/n): "
+set /p AutoYes="Automatically say yes to all prompts? (y/n): "
+set /p ForceOption="Force all choco installations? (y/n): "
+
+if /I "%AutoYes%" == "y" (
+	set "AutoYes=y"
+) else (
+	set AutoYes=
+)
+if /I "%ForceOption%" == "y" (
+	set "ForceOption=f"
+) else (
+	set ForceOption=
+)
+set OptionText=-%ForceOption%%AutoYes%
+if "%OptionText%" == "-" (
+	set OptionText=
+)
+
+choco install ctags %OptionText%
+choco install curl %OptionText%
+choco install git %OptionText%
+choco install python --version 3.7.3 %OptionText%
 python -m pip install --upgrade pip
 python -m pip install python-language-server
-choco install anaconda3 -fy
-choco install vim -fy
-choco install cmake -fy
-choco install sourcetree -fy
-choco install sql-server-management-studio -fy
-choco install putty -fy
-choco install filezilla -fy
-choco install greenshot -fy
+choco install vim %OptionText%
+choco install putty %OptionText%
 
+if /I "%BasicInstall%" == "n" (
+	choco install chocolateygui %OptionText%
+	choco install cmake %OptionText%
+	choco install sourcetree %OptionText%
+	choco install sql-server-management-studio %OptionText%
+	choco install filezilla %OptionText%
+	choco install greenshot %OptionText%
+)
+copy %~dp0MyVimRC %userprofile%\_vimrc
 cd %USERPROFILE%
-git clone https://github.com/VundleVim/Vundle.vim.git:q %USERPROFILE%/vimfiles/bundle/Vundle.vim
+if not exist %userprofile%\vimfiles\bundle\Vundle.vim (
+	git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%\vimfiles\bundle\Vundle.vim
+)
+gvim +PluginInstall +qall
+PAUSE 2>nul
+EXIT /B 0
